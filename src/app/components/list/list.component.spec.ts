ecaa7d10783e1eab96e120a863e1d72ba1a382ab
dmitryprogrammer <dpischalka@gmail.com>
2023-12-14 01:55:55 +0400

    Tested commit signature by fixed spec for list component.

--- src/app/components/list/list.component.spec.ts
+++ src/app/components/list/list.component.spec.ts
@@ -1,15 +1,17 @@
+import {CommonModule} from '@angular/common';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {ListComponent} from './list.component';
+import {SharedModule} from '../../../shared/shared.module';
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ListComponent]
-    });
+    TestBed.configureTestingModule({imports: [BrowserAnimationsModule, HttpClientTestingModule, CommonModule, SharedModule, ListComponent]}).compileComponents();
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
